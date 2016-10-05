5407cac19aee1e70a8a417e7725562a8cd441ef2
Tiberiu Covaci <tibi@covaci.se>
2016-10-05 23:24:48 +0100

    Add http service with promise

--- app/recipe/recipe.module.ts
+++ app/recipe/recipe.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import { RecipeService } from './recipe.service';
 import { ImagePipeTransform } from './image.pipe';
 import { WeeklyMenuComponent }   from './weekly-menu.component';
 import { RecipeDetailsComponent }   from './recipe-details.component';
+import 'rxjs/Rx';
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule, HttpModule],
     exports: [WeeklyMenuComponent],
     declarations: [WeeklyMenuComponent, RecipeDetailsComponent, ImagePipeTransform],
     providers: [RecipeService],
--- app/recipe/recipe.service.ts
+++ app/recipe/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
 
 export interface IRecipe {
     recipeId: number;
@@ -7,86 +8,17 @@ export interface IRecipe {
     time: number;
     image: string;
     dayOfWeek?: string;
+    recipeIngredients?: any[];
+    recipeInstructions?: any[];
 }
 
 @Injectable()
 export class RecipeService {
 
-    constructor() { }
+    constructor(private _http: Http) { }
 
     getRecipes(): Promise<IRecipe[]> {
-        return Promise.resolve([{
-            "recipeId": 1,
-            "name": "Fish sticks and Rice",
-            "price": 2.0,
-            "time": 20,
-            "image": "fishsticks-mine",
-            "recipeIngredients": [],
-            "recipeInstructions": []
-        }, {
-            "recipeId": 2,
-            "name": "Pizza",
-            "price": 13.0,
-            "time": 30,
-            "image": "pizza-clip",
-            "recipeIngredients": [],
-            "recipeInstructions": []
-        }, {
-            "recipeId": 3,
-            "name": "Pasta with Meatsauce",
-            "price": 3.0,
-            "time": 11,
-            "image": "pasta-clip",
-            "recipeIngredients": [],
-            "recipeInstructions": []
-        }, {
-            "recipeId": 4,
-            "name": "Cereal with Millk",
-            "price": 1.0,
-            "time": 1,
-            "image": "cereal-clip",
-            "recipeIngredients": [],
-            "recipeInstructions": []
-        }, {
-            "recipeId": 5,
-            "name": "Meatballs and Mashed Potatoes",
-            "price": 4.0,
-            "time": 5,
-            "image": "meatballs-clip",
-            "recipeIngredients": [],
-            "recipeInstructions": []
-        }, {
-            "recipeId": 6,
-            "name": "Waffles and Jam",
-            "price": 3.0,
-            "time": 12,
-            "image": "waffles-clip",
-            "recipeIngredients": [],
-            "recipeInstructions": []
-        }, {
-            "recipeId": 7,
-            "name": "Hamburgers",
-            "price": 3.0,
-            "time": 15,
-            "image": "hamburger-clip",
-            "recipeIngredients": [],
-            "recipeInstructions": []
-        }, {
-            "recipeId": 8,
-            "name": "Ham and Cheeze Sandwich",
-            "price": 2.0,
-            "time": 5,
-            "image": "sandwich-clip",
-            "recipeIngredients": [],
-            "recipeInstructions": []
-        }, {
-            "recipeId": 9,
-            "name": "Magic Ice cream",
-            "price": 4.0,
-            "time": 3,
-            "image": "icecream-clip",
-            "recipeIngredients": [],
-            "recipeInstructions": []
-        }]);
+        return this._http.get('/Artifacts/data/recipies.json')
+                    .toPromise().then(r => r.json() as IRecipe[]);
     }
 }
