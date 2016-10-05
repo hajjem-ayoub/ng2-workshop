d76fcebd65ddada5686ce32fa6c16c92aa9133d6
Tiberiu Covaci <tibi@covaci.se>
2016-10-05 22:49:17 +0100

    Added the recipe service

--- app/recipe/recipe.module.ts
+++ app/recipe/recipe.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RecipeService } from './recipe.service';
 import { WeeklyMenuComponent }   from './weekly-menu.component';
 
 @NgModule({
     imports: [BrowserModule],
     exports: [WeeklyMenuComponent],
     declarations: [WeeklyMenuComponent],
-    providers: [],
+    providers: [RecipeService],
 })
 export class RecipeModule { }
