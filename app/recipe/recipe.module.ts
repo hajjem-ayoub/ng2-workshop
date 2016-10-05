1c01954953f76eb5fd24b06cc5c1cefa1ed42930
Tiberiu Covaci <tibi@covaci.se>
2016-10-05 22:58:00 +0100

    Pipe created

--- app/recipe/recipe.module.ts
+++ app/recipe/recipe.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { RecipeService } from './recipe.service';
+import { ImagePipeTransform } from './image.pipe';
 import { WeeklyMenuComponent }   from './weekly-menu.component';
 
 @NgModule({
     imports: [BrowserModule],
     exports: [WeeklyMenuComponent],
-    declarations: [WeeklyMenuComponent],
+    declarations: [WeeklyMenuComponent, ImagePipeTransform],
     providers: [RecipeService],
 })
 export class RecipeModule { }
--- app/recipe/recipe.service.ts
+++ app/recipe/recipe.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+
 export interface IRecipe {
     recipeId: number;
     name: string;
